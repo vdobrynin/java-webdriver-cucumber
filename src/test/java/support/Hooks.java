9dd62b6e40da35c585980c36455bf7f21aec9f89
Slava Skryabin <vs8199@gmail.com>
2019-07-06 15:47:55 -0700

    • Updating to Java 11 • Grid support • REST Assured

--- src/test/java/support/Hooks.java
+++ src/test/java/support/Hooks.java
@@ -6,21 +6,23 @@ import cucumber.api.java.Before;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import static support.TestContext.getDriver;
+
 public class Hooks {
 
     @Before(order = 0)
     public void scenarioStart() {
         TestContext.initialize();
-        TestContext.getDriver().manage().deleteAllCookies();
+        getDriver().manage().deleteAllCookies();
     }
 
     @After(order = 0)
     public void scenarioEnd(Scenario scenario) {
         if (scenario.isFailed()) {
-            TakesScreenshot screenshotTaker = (TakesScreenshot) TestContext.getDriver();
+            TakesScreenshot screenshotTaker = (TakesScreenshot) getDriver();
             byte[] screenshot = screenshotTaker.getScreenshotAs(OutputType.BYTES);
             scenario.embed(screenshot, "image/png");
         }
-        TestContext.close();
+        TestContext.teardown();
     }
 }
--- src/test/java/support/TestRunner.java
+++ src/test/java/support/TestRunner.java
@@ -10,8 +10,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber/report.json"},
         features = "src/test/resources/features",
-        glue = {"definitions", "support"}//,
-//        tags = {"@predefined1"} // same as VM option -Dcucumber.options="--tags @predefined1"
+        glue = {"definitions", "support"},
+        tags = {"@predefined"} // same as VM option -Dcucumber.options="--tags @predefined"
 )
 public class TestRunner {
     @BeforeClass
