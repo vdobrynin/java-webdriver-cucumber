8babfde929ba842c19505c357a40fb3dc7bf33e2
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2023-10-12 23:38:00 -0700

    refactor quoteForm again

--- src/test/java/pages/QuoteForm.java
+++ src/test/java/pages/QuoteForm.java
@@ -20,10 +20,8 @@ public class QuoteForm extends Page {
     private WebElement password;
     @FindBy(xpath = "//input[@id='confirmPassword']")
     private WebElement confirmPassword;
-
     @FindBy(xpath = "//input[@id='name']")
     private WebElement name;
-
 //                                                // ---> Name dialog - start -
     @FindBy(xpath = "//input[@id='firstName']")
     private WebElement firstName;
@@ -34,20 +32,19 @@ public class QuoteForm extends Page {
     @FindBy(xpath = "//span[text()='Save']")
     private WebElement saveButton;
 //                                                // ---> Name dialog - end -
-
     @FindBy(xpath = "//input[@name='agreedToPrivacyPolicy']")
     private WebElement privacy;
     @FindBy(xpath = "//button[@id='formSubmit']")
     private WebElement submitButton;                // ---> submit
 
     public void clickSubmit() {
-        //        submitButton.click();                                                     //--> first solution will not work
-        getExecutor().executeScript("arguments[0].click();", submitButton);           // see above like PrivacyPolicy
+        //        submitButton.click();                                         //--> first solution will not work
+        getExecutor().executeScript("arguments[0].click();", submitButton); // see above like PrivacyPolicy
     }
 
     public void clickPrivacy() {
-        //        privacy.click();                                                    //--> first solution will not work
-        getExecutor().executeScript("arguments[0].click();", privacy);          // it is better to use JavaScript
+        //        privacy.click();                                          //--> first solution will not work
+        getExecutor().executeScript("arguments[0].click();", privacy);// it is better to use JavaScript
     }
 
     public void fillName(String firstName, String lastName) {
@@ -68,22 +65,25 @@ public class QuoteForm extends Page {
     }
 
     public void fillUsername(String value) {
+
         username.sendKeys(value);
     }
 
     public void fillEmail(String value) {
+
         email.sendKeys(value);
     }
 
     public void fillPassword(String value) {
+
         password.sendKeys(value);
     }
 
     public void fillConfirmPassword(String value) {
+
         confirmPassword.sendKeys(value);
     }
 
     public void fillOutRequiredFields() {
     }
-
 }
