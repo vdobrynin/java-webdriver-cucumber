b17c47d5241361598bac48285add39271ee96c93
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2023-10-24 00:07:24 -0700

    quoteform change

--- src/test/java/pages/QuoteResult.java
+++ src/test/java/pages/QuoteResult.java
@@ -1,7 +1,7 @@
 package pages;
 
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.*;
 
 public class QuoteResult extends Page {
 
