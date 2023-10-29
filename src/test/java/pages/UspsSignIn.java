a1488a8f00b117107d46414b7e4963fa887fe6e8
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2023-10-29 03:07:49 -0700

    fix xpath's for @usps_object3 headless

--- src/test/java/pages/UspsSignIn.java
+++ src/test/java/pages/UspsSignIn.java
@@ -16,13 +16,11 @@ import static support.TestContext.getDriver;
 
 public class UspsSignIn extends Page {
 
-    @FindBy(xpath = "(//img[@id='usps-logo'])[1]")
-    private WebElement logo;
-    @FindBy(xpath = "(//a[@href='/entreg/RegistrationAction_input'][contains(.,'Sign Up Now')])[1]")
+    @FindBy(xpath = "//a[@id='sign-up-button']")
     private WebElement signUpButton;
-    @FindBy(xpath = "(//button[normalize-space()='Sign In'])[1]")
+    @FindBy(xpath = "//button[@id='btn-submit']")
     private WebElement signInButton;
-    @FindBy(xpath = "(//span[@class='error-txt-blk'][normalize-space()='A Username is a required entry.'])[1]")
+    @FindBy(xpath = "//span[@id='error-username']")
     private WebElement usernameError;
 
     public boolean isSignUpPossible() {
@@ -37,23 +35,17 @@ public class UspsSignIn extends Page {
             .pollingEvery(Duration.ofSeconds(5))
             .ignoring(NoSuchElementException.class);
 
-        fluentWait.until(ExpectedConditions.visibilityOf(signInButton));
         Actions action = new Actions(getDriver());
-        action.moveToElement(signInButton)
-            .click(signInButton)
-            .build()
-            .perform();
+        fluentWait.until(ExpectedConditions.visibilityOf(signInButton));
+        signInButton.click();
 
         try {
+            fluentWait.until(ExpectedConditions.visibilityOf(signInButton));
             action.moveToElement(signInButton)
                 .click(signInButton)
                 .build()
                 .perform();
-//            action.moveToElement(signInButton)
-//                .moveToElement(logo)
-//                .build()
-//                .perform();
-//            signInButton.click();
+
             fluentWait.until(ExpectedConditions.visibilityOf(usernameError));
             return true;
         } catch (TimeoutException e) {
